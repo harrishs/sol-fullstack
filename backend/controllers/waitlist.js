6bc106d0681032a33e81d4f3dc009382a3702267
Harrish Suhumar <harrish.s@hotmail.ca>
2022-11-25 20:57:38 -0500

    login route

--- backend/controllers/waitlist.js
+++ backend/controllers/waitlist.js
@@ -19,7 +19,26 @@ exports.postRegister = (req, res, next) => {
     .then((waitlist) => {
       res.status(201).json({ waitlist });
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => res.status(400).json({ err }));
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedWaitlist;
+  Waitlist.findOne({ email: email })
+    .then((waitlist) => {
+      if (!waitlist) {
+        res.status(401).json({ err: "Email not found" });
+      }
+      loadedWaitlist = waitlist;
+      return bcrypt.compare(password, waitlist.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        res.status(401).json({ err: "Wrong password" });
+      }
+      res.status(200).json({ waitlist: loadedWaitlist });
+    })
+    .catch((err) => res.status(401).json({ err }));
 };
--- backend/routes/waitlist.js
+++ backend/routes/waitlist.js
@@ -5,5 +5,6 @@ const waitlistController = require("../controllers/waitlist");
 const router = express.Router();
 
 router.post("/register", waitlistController.postRegister);
+router.post("/login", waitlistController.postLogin);
 
 module.exports = router;
